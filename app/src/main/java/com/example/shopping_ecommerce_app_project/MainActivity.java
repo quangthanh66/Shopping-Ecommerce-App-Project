5fa0e26ad6b7edbb4bf103a5829ceec013922a09
quangthanh66 <quangthanhbt66@gmail.com>
2023-02-22 14:35:33 +0700

    Started

---
+++ app/src/main/java/com/example/shopping_ecommerce_app_project/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.shopping_ecommerce_app_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
